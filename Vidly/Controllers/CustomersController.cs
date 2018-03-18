55f3e5b4b631b1f9a37d4b86d4cfe513c53329bf
Sachin Sharma <sachinksharma@users.noreply.github.com>
2018-03-17 19:52:42 -0500

    Added Edit and Update functionality for customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -37,5 +38,51 @@ namespace Vidly.Controllers
                 return HttpNotFound();
         }
 
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var existingcustomer = _context.Customers.Single(c => c.Id == customer.Id);
+                existingcustomer.Name = customer.Name;
+                existingcustomer.DateOfBirth = customer.DateOfBirth;
+                existingcustomer.IsSubscribledToNewsLetter = customer.IsSubscribledToNewsLetter;
+                existingcustomer.MemberShipTypeId = customer.MemberShipTypeId;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
+
+
+        public ActionResult Save()
+        {
+            var membershiptypes = _context.MembershipTypes.ToList();
+            var newcustomerviewmodel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershiptypes,
+            };
+            return View("CustomerForm", newcustomerviewmodel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer != null)
+            {
+                var viewmodel = new CustomerFormViewModel()
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewmodel);
+            }
+
+            else
+                return HttpNotFound();
+        }
     }
 }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -6,14 +6,20 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        [Display(Name = "Date of Birth")]
         public DateTime DateOfBirth { get; set; }
+
         public bool IsSubscribledToNewsLetter { get; set; }
 
         public MembershipType MembershipType { get; set; }
+        [Display(Name = "Membership Type")]
         public byte MemberShipTypeId { get; set; }
     }
 }
