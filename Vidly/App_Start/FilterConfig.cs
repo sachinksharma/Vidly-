d6e9faab9321401c05ee5ff13322472a38487dba
Sachin Sharma <sachinksharma@users.noreply.github.com>
2018-04-01 01:37:32 -0500

    Final Complete

--- Vidly/App_Start/FilterConfig.cs
+++ Vidly/App_Start/FilterConfig.cs
@@ -7,7 +7,8 @@ namespace Vidly
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new AuthorizeAttribute());
+            filters.Add(new AuthorizeAttribute());     // Authorize attribute 
+            filters.Add(new RequireHttpsAttribute());  // Enable HTTPS
         }
     }
 }
